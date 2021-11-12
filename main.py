60346f7140831e794954b7301a7d4e56accd01b4
Alik <90277937+zheskod@users.noreply.github.com>
2021-11-12 23:10:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
-from random import *
-
-def eng_code(text, step):
+# Функция для шфировки/дешифровки на английском языке
+def eng_code(change, text, step):
     eng_lower_alphabet = 'abcdefghijklmnopqrstuvwxyz'
     eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = []
+    for i in range(len(text)):
+        if text[i].isalpha():
+            if text[i].isupper():
+                new_text.append(eng_upper_alphabet[(eng_upper_alphabet.find(text[i]) + step) % 26])
+            elif text[i].islower():
+                new_text.append(eng_lower_alphabet[(eng_lower_alphabet.find(text[i]) + step) % 26])
+        else:
+            new_text.append(text[i])
+    print(*new_text, sep='')
+    return
 
-def ru_code(text, step):
+# Функция для шфировки/дешифровки на русском языке
+def ru_code(change, text, step):
     rus_lower_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
     rus_upper_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+    new_text = []
+    for i in range(len(text)):
+        if text[i].isalpha():
+            if text[i].isupper():
+                new_text.append(rus_upper_alphabet[(rus_upper_alphabet.find(text[i]) + step) % 32])
+            elif text[i].islower():
+                new_text.append(rus_lower_alphabet[(rus_lower_alphabet.find(text[i]) + step) % 32])
+        else:
+            new_text.append(text[i])
+    print(*new_text, sep='')
+    return
+
 
 # приветствие
-print("Caesar 's Cipher v.1.0. приветствует тебя!")
+print("Caesar's Cipher v.1.0. приветствует тебя!")
 
 # вопрос 1
-input('Будем шифровать (ш) или дешифровать (д) ?: ')
+change_encryption = input('Будем шифровать (ш) или дешифровать (д) ?: ')
 
 # выбор языка
-input('На каком языке будем шфировать, английский (а) или русский (р)?: ')
+text_main = input('На каком языке будем шфировать, английский (а) или русский (р)?: ')
 
 # шаг сдвига
 shift_step = int(input('Ключ? (шаг сдвига): '))
+
+# текст шифровки/расшифровки
+code_txt = input('Введите текст: ')
+
+if text_main == 'р':
+    ru_code(change_encryption, code_txt, shift_step)
+else:
+    eng_code(change_encryption, code_txt, shift_step)
